c91371dacbeaa250d329bd97bc8fa5ac502c3033
NodjaLima <nodjamplima@gmail.com>
2022-04-20 12:16:46 -0300

    Corrige erro de texto na fase 3 de bastet

--- bastet/fase3bastet.js
+++ bastet/fase3bastet.js
@@ -3,7 +3,7 @@ function faseTres () {
   var respostaTres = prompt("1 - Quando você é um rato; 2 - Quando estiver embaixo da escada; 3 - A noite.");
   
   if (respostaTres == 1) {
-    alert ("Você foi simplesmente genial. As esfinges não tiveram chance. Annubis está muito grato.");
+    alert ("Você foi simplesmente genial. As esfinges não tiveram chance. Bastet está muito grata.");
     window.location.href= "../vitoria.html";
   } else if (respostaTres == 3 || respostaTres == 2) {
     window.location.href= "../gameover.html";
