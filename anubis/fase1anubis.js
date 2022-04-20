ab510fdb0553487650c477155d5c24a0d56ea581
NodjaLima <nodjamplima@gmail.com>
2022-04-20 01:30:26 -0300

    Reparando links internos

--- anubis/fase1anubis.js
+++ anubis/fase1anubis.js
@@ -7,7 +7,7 @@ if (respostaUm == 2) {
   alert ("Muito bem. A primeira já foi.");
   window.location.href= "./fase2anubis.html";
 } else if (respostaUm == 1 || respostaUm == 3) {
-  window.location.href = "./../../../gameover.html";
+  window.location.href = "../../../../gameover.html";
 } else {
   alert("Opção Inválida");
   faseUm();
